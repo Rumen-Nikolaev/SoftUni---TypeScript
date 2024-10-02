ae6b6a5549f7c382a6758ba529cc4787ead42796
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2024-10-02 18:04:03 +0300

    Update findLargestNumber.ts

--- 2. Introduction to TypeScript/Exercises/# 2. Largest Number/findLargestNumber.ts
+++ 2. Introduction to TypeScript/Exercises/# 2. Largest Number/findLargestNumber.ts
@@ -2,7 +2,7 @@ function findLargestNumber(num1: number, num2: number, num3: number): void {
     let largest = num1;
 
     if (num2 > largest) {
-        largest: num2;
+        largest = num2;
     }
 
     if (num3 > largest) {
